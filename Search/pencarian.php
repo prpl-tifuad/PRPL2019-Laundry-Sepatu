33b6323a799096b5de0da8faa74961bee687b4a9
Dimas Aji Setiawan <40574883+das1997@users.noreply.github.com>
2019-04-06 17:10:44 +0700

    Add files via upload

---
+++ Search/pencarian.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Form Pencarian</title>
+</head>
+<body>
+<p align="center"><strong>Form Pencarian Data Pemesanan</strong></p>
+<form method="post" action="" class="search" >
+<table align="center" border="0">
+<tr>
+<td>
+<input size="45" name="Pencarian" id="Pencarian" type="text" placeholder="Masukkan Pencarian">
+<input class="submit" type="submit" name="cari" value="CARI" >
+</td>
+</tr>
+</table>
+</form>
+      <table width="600" border="1" align="center" cellpadding="1" cellspacing="0">
+        <tr bgcolor="#FF9999">
+          <td><div align="center">Tanggal</div></td>
+          <td><div align="center">Paket</div></td>
+          <td><div align="center">Total</div></td>
+        </tr>
+</table>
+</body>
+</html>
