517bd98d890b23cf83d06013fe2b34af567f1fe9
Azmi Badhi'uz Zaman <40100306+azmi1700018002@users.noreply.github.com>
2019-04-11 21:39:50 +0700

    Project

---
+++ Login/config.php
@@ -0,0 +1,3 @@
+<?php
+mysqli_connect('localhost','root','','malasngoding_login')or die("gagal, database tidak ditemukan");
+?>
---
+++ Login/login.php
@@ -0,0 +1,19 @@
+<?php 
+include 'config.php';
+ 
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+ 
+$login = mysql_query("select * from user where username='$username' and password='$password'");
+$cek = mysql_num_rows($login);
+ 
+if($cek > 0){
+	session_start();
+	$_SESSION['username'] = $username;
+	$_SESSION['status'] = "login";
+	header("location:admin/index.php");
+}else{
+	header("location:index.php");	
+}
+ 
+?>
